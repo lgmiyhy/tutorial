9f44d07c82cdc6605e9d1b51dd654fc83f50f795
lgmiyhy <43487496+lgmiyhy@users.noreply.github.com>
2018-10-22 16:45:17 +0900

    auto commit

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -23,13 +23,13 @@ Including another URLconf
 
 from django.urls import path, include
 from django.contrib import admin
-from tutorial import views
-from django.conf import settings
-from django.conf.urls.static import static
+#from tutorial import views
+#from django.conf import settings
+#from django.conf.urls.static import static
 
 urlpatterns = [
 #    path('', views.login_redirect, name='login_redirect'),
     path('admin/', admin.site.urls),
-    path('account/', include('accounts.urls'))
+    path('account/', include('accounts.urls')),
 #    path('home/', include('home.urls', namespace='home')),
 ]
