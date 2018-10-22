985b651907a3f759a5600e0414e5dcfc9aceef59
lgmiyhy <43487496+lgmiyhy@users.noreply.github.com>
2018-10-22 10:55:51 +0900

    following Django Lesson 6

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponse
 
 def home(request):
+    numbers = [1,2,3,4,5]
+    name = 'Max Goodridge'
+    args = {'myName':name, 'numbers':numbers}
 #    return HttpResponse('Home Page!!!')
-    return render(request, 'accounts/login.html')
+    return render(request, 'accounts/login.html', args)
