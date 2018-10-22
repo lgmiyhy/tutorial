95cbabae6e588f714e66922e758565c3b03611de
lgmiyhy <43487496+lgmiyhy@users.noreply.github.com>
2018-10-22 15:01:35 +0900

    auto commit

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -13,10 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+#from django.contrib import admin
+#from django.urls import path, include
+#
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#    path('account/',include('accounts.urls')),
+#]
+
 from django.urls import path, include
+from django.contrib import admin
+from tutorial import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+#    path('', views.login_redirect, name='login_redirect'),
     path('admin/', admin.site.urls),
-    path('account/',include('accounts.urls')),
+    path('account/', include('accounts.urls')),
+#    path('home/', include('home.urls', namespace='home')),
 ]
