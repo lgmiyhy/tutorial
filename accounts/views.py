23bfcb686a08c82dafd0effbe3aeb121893d30b2
lgmiyhy <43487496+lgmiyhy@users.noreply.github.com>
2018-10-22 15:01:19 +0900

    auto commit

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 def home(request):
 #    return HttpResponse('Home Page!!!')
-    return render(request, 'accounts/login.html')
+    return render(request, 'accounts/home.html')
