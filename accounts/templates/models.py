53c00583b17e0f15990d4fea7f2579b4f03c333f
lgmiyhy <43487496+lgmiyhy@users.noreply.github.com>
2018-10-22 15:59:47 +0900

    auto commit;

--- accounts/templates/models.py
+++ accounts/templates/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auto.models import User
+from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     description = models.CharField(max_length=100, default='')
-    city = models.CharField(max_length=100,default='')
+    city = models.CharField(max_length=100, default='')
     website = models.URLField(default='')
     phone = models.IntegerField(default=0)
